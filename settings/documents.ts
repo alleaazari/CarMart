3dc0d82ed57b81b7b8c5df661ba01cf672ab5ac2
alleaazari <alleaazarilubis@gmail.com>
2025-01-15 00:01:40 +0700

    rrr

--- settings/documents.ts
+++ settings/documents.ts
@@ -7,7 +7,7 @@ export const Documents: Paths[] = [
     heading: "Getting started",
     items: [
       {
-        title: "Installation",
+        title: "Fitur Utama",
         href: "/installation",
       },
       {
